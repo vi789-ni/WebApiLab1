7837d4359e33b0cc909cd952d17b0ae00804ba0e
nikif <nikif@WINX>
2026-09-11 16:52:18 +0500

    Добавьте файлы проекта.

---
+++ WebApiLab1/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiLab1.Models;
+
+namespace WebApiLab1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private static readonly List<Student> students = new()
+        {
+            new Student { Id = 1, Name = "Ayan", Age = 20 },
+            new Student { Id = 2, Name = "Dana", Age = 21 }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Student>> GetStudents()
+        {
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> GetStudentById(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public ActionResult<Student> AddStudent(Student student)
+        {
+            student.Id = students.Count + 1;
+            students.Add(student);
+            return Ok(student);
+        }
+    }
+}
---
+++ WebApiLab1/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiLab1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
